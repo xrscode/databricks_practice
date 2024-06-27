cfa15f84d48d2e0f0ae978e719afb5d5071ca633
xrscode <120644276+xrscode@users.noreply.github.com>
2024-06-27 16:07:30 +0100

    updated terraform config files

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -25,3 +25,15 @@ resource "azurerm_storage_data_lake_gen2_path" "raw_directory" {
     storage_account_id = azurerm_storage_account.data_lake.id
     resource = "directory"
 }
+
+# Uplade JSON to RAW directory:
+resource "azurerm_storage_blob" "json" {
+    name = "RAW/dbdata.json"
+    storage_account_name   = azurerm_storage_account.data_lake.name
+    storage_container_name = azurerm_storage_data_lake_gen2_filesystem.my_lake.name
+    type                   = "Block"  # Specify the blob type as "Block"
+    source                 = "../data/dbdata.json"  # Path to JSON file
+    depends_on = [
+        azurerm_storage_data_lake_gen2_path.raw_directory
+  ]
+}
