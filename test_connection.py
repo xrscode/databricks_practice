99800eca95e43717e4db44d5d9a0c622cd65b2d6
xrscode <120644276+xrscode@users.noreply.github.com>
2024-06-28 17:36:16 +0100

    Updated terraform config

---
+++ test_connection.py
@@ -0,0 +1,22 @@
+import pyodbc
+
+# # Connection parameters:
+# server = 'databricks-practice-server.database.windows.net'
+# database = 'databricks-practice-server.database.windows.net'
+# username = 'sqladm'
+# password = 'Dylan5761!'
+# # Use the correct ODBC driver version
+# driver = '{ODBC Driver 17 for SQL Server}'
+
+# # Test connection to database
+# try:
+#     # Establish connection
+#     conn = pyodbc.connect(f'DRIVER={driver};SERVER={server};'
+#                           f'DATABASE={database};UID={username};PWD={password}')
+#     print("Connection successful!")
+
+#     # Close the connection
+#     conn.close()
+
+# except Exception as e:
+#     print(f"Connection failed: {str(e)}")
